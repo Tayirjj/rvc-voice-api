ccc0b3fc434883cc7af8e4e6a32b9eab88099d55
Tayirj <farouksaif980@gmail.com>
2026-02-20 05:52:36 +0300

    Fix audio_base64 handling in preprocess function

--- app.py
+++ app.py
@@ -241,15 +241,18 @@ def preprocess():
     try:
         data = request.get_json()
         user_id = data.get('user_id')
+        audio_base64 = data.get('trainset_dir')
+        audio_bytes = base64.b64decode(audio_base64)
+
         doc_data = {
-            "audio_base64" : data.get('trainset_dir'),
+            "audio_base64" : audio_base64,
             "exp_dir" : data.get('exp_dir1'),
             "sr" : data.get('sr'),
             "n_p" : data.get('n_p'),
             "user_id" : data.get('user_id'),
         }
 
-        audio_bytes = base64.b64decode(audio_base64)
+        
 
         response = requests.post(
             f"{COLAB_URL}/preprocess",
@@ -271,6 +274,9 @@ def preprocess():
 
     except Exception as d:
         print(f"Error is {d}")
+        
+
+    
         
 
         
