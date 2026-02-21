0e0848ac8281b65ab7d89ac0325df4dfcb7e2e9b
Tayirj <farouksaif980@gmail.com>
2026-02-21 05:32:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -302,7 +302,7 @@ def add_to_favorite():
                 db.collection('training_voices').document(user_id).collection(exp_dir).update({"is_favorite" : is_favorite})
                 return jsonify({"messege" : "add to favorite is sucessfull" , "status" : "True"})
             except Exception as f:
-                return jsonify({"messege" : "add to favorite is Error : {f}" , "status" : "False"})
+                return jsonify({f"messege" : "add to favorite is Error : {f}" , "status" : "False"})
 
     except Exception as d:
         print(f'error:{d}')
